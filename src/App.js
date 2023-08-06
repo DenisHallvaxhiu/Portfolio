aa84cf6c18c85cfbfb698cbc8a001eecfee4b2ac
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2023-08-06 17:57:45 -0400

    App layout in website,flex display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Background from "./assets/Blue Ribbon Window 11.jpg"
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className='app'>
       <img src={Background}/>
+      <Main/>
       <Footer/>
     </div>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./Main.css"
+
+export default function Main() {
+  return (
+    <main className='main'>
+        <div className='app-container'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </main>
+  )
+}
