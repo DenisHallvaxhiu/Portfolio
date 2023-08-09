86a715234053dc8e4cf49843e1f8f10e693f0ef4
Denis Hallvaxhiu <dhallvaxhiu@serviceexpress.com>
2023-08-09 00:20:12 -0400

    Loading in modal,and close button

--- src/components/ProjectsModal/ProjectsModal.jsx
+++ src/components/ProjectsModal/ProjectsModal.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProjectsModal.css";
 import { motion, AnimatePresence } from "framer-motion";
+import { PacmanLoader } from "react-spinners";
 
 export default function ProjectsModal({ open, close }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+  }, [open]);
+
   return (
     <AnimatePresence>
       {open && (
@@ -12,30 +24,17 @@ export default function ProjectsModal({ open, close }) {
           exit={{ scale: 0 }}
           transition={{ ease: "easeOut", duration: 0.2 }}
           className="modal"
-          onClick={close}
         >
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-              eius ducimus velit inventore. Id vel cupiditate, similique
-              asperiores eius accusantium aspernatur suscipit eum quos tenetur
-              nesciunt labore numquam iure perferendis. Eveniet voluptas
-              doloribus, eos necessitatibus itaque asperiores earum, soluta
-              autem neque esse recusandae accusamus corrupti. A rerum harum enim
-              iusto quidem laudantium asperiores iure magni doloribus!
-              Voluptates non nobis eius. Dignissimos quibusdam eum tenetur unde
-              porro alias tempora praesentium dicta possimus eius enim sint
-              aperiam perspiciatis, consequuntur soluta quas, nihil
-              reprehenderit dolor nostrum dolore non nulla, assumenda
-              repellendus! Repellat, reiciendis. Reiciendis dolorem dolor
-              voluptatibus, fugit omnis cupiditate illo recusandae quibusdam
-              voluptatum ad beatae ullam sunt quisquam nulla eligendi sapiente
-              voluptate saepe deleniti at est? Qui, unde voluptatem! Fuga, ab
-              numquam! Voluptatem, unde itaque molestiae eveniet excepturi aut
-              consectetur sapiente laudantium harum aliquam? Quam libero illo
-              porro blanditiis fugit nesciunt incidunt reprehenderit cum aperiam
-              asperiores quas, sit suscipit molestiae. Aliquam, a.
-            </p>
+          <button onClick={close} type="button" className="btn-close">
+            <span className="icon-cross"></span>
+            <span className="visually-hidden">X</span>
+          </button>
+          <div className="modal-div">
+            {loading ? (
+              <PacmanLoader size={40} color="#36d7b7" />
+            ) : (
+              <div className="test"></div>
+            )}
           </div>
         </motion.div>
       )}
