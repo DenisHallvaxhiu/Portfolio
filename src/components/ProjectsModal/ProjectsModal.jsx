3e69e9dba29d41e788f1167863387feb6391b157
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2023-08-09 23:01:27 -0400

    My project page finished with test data

--- src/components/ProjectsModal/ProjectsModal.jsx
+++ src/components/ProjectsModal/ProjectsModal.jsx
@@ -7,6 +7,13 @@ import NBA from "../../assets/nba-logo.jpg";
 import NBAImage from "../../assets/nba-image.avif"
 import PGALogo from "../../assets/PGA_Tour_logo.png"
 import PGAImage from "../../assets/PGA_Tour_image.jpg"
+import MLBLogo from "../../assets/MLB.png"
+import MLBImage from "../../assets/MLB_Image.png"
+import SoccerLogo from "../../assets/Soccer.png"
+import SoccerImage from "../../assets/Soccer_Image.webp"
+import NFL from "../../assets/NFL.svg"
+import NFLImage from "../../assets/NFL_image.webp"
+
 
 export default function ProjectsModal({ open, close }) {
   const [loading, setLoading] = useState(false);
@@ -23,9 +30,9 @@ export default function ProjectsModal({ open, close }) {
   const ProjectCards = [
     {logo:NBA,image:NBAImage, header: "National Basketball Association", topic:"Sports", type:"Website Application"},
     {logo:PGALogo,image:PGAImage, header: "PGA TOUR", topic:"Sports", type:"Website Application"},
-    {logo:NBA,image:NBAImage, header: "National Basketball Association", topic:"Sports", type:"Website Application"},
-    {logo:NBA,image:NBAImage, header: "National Basketball Association", topic:"Sports", type:"Website Application"},
-    {logo:NBA,image:NBAImage, header: "National Basketball Association", topic:"Sports", type:"Website Application"},
+    {logo:MLBLogo,image:MLBImage, header: "Major League Baseball", topic:"Sports", type:"Website Application"},
+    {logo:SoccerLogo,image:SoccerImage, header: "UEFA Champions League", topic:"Sports", type:"Website Application"},
+    {logo:NFL,image:NFLImage, header: "National Football League", topic:"Sports", type:"Website Application"},
   ]
 
 
