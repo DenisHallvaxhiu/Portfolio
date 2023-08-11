ee6b58f3a61b9fb7057d14a12e89e0619744cb45
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2023-08-11 01:16:06 -0400

    Start menu functionality, project info mvp

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import "./App.css";
 import Background from "./assets/Blue Ribbon Window 11.jpg";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
+import Project from "./assets/Projects.png";
+import Resume from "./assets/PDFResume.png";
+import Github from "./assets/Github.png";
+import LinkedIn from "./assets/LinkedIn.png";
+import ResumePdf from "./assets/Resume/Resume+Denis+Hallvaxhiu.pdf";
 
 function App() {
   const [startMenu, setStartMenu] = useState(true);
-  const [startMenuExpanded,setStartMenuExpanded] = useState(false)
+  const [startMenuExpanded, setStartMenuExpanded] = useState(false);
   let startMenuRef = useRef();
 
   useEffect(() => {
@@ -15,6 +20,7 @@ function App() {
       if (startMenuRef.current) {
         if (!startMenuRef.current.contains(e.target)) {
           setStartMenu(false);
+          setStartMenuExpanded(false);
         }
       }
     };
@@ -25,6 +31,20 @@ function App() {
       document.removeEventListener("mousedown", handler);
     };
   }, []); // Empty dependency array to ensure the effect runs only once
+
+  const openLinkedInPage = () => {
+    window.open(
+      "https://www.linkedin.com/in/denis-hallvaxhiu-0bb86a226/",
+      "_blank"
+    );
+  };
+  const openGithubPage = () => {
+    window.open("https://github.com/DenisHallvaxhiu", "_blank");
+  };
+
+  const openResumeInPage = () => {
+    window.open(ResumePdf, "_blank");
+  };
 
   return (
     <div className="app">
@@ -43,9 +63,33 @@ function App() {
             transition={{ duration: 0.5 }}
             className="startMenu-container"
           >
-            <div style={{height: startMenuExpanded ? '600px' : '200px'}} ref={startMenuRef} className="startMenu">
-              <div className="upper"></div>
-              <div className="lower" onClick={()=>setStartMenuExpanded(!startMenuExpanded)}></div>
+            <div
+              style={{ height: startMenuExpanded ? "600px" : "180px" }}
+              ref={startMenuRef}
+              className="startMenu"
+            >
+              <div className="upper">
+                <div className="applications">
+                  <img src={Project} alt="" />
+                  <h2>Project</h2>
+                </div>
+                <div className="applications">
+                  <img src={Resume} alt="" />
+                  <h2>Resume</h2>
+                </div>
+                <div className="applications">
+                  <img src={Github} alt="" />
+                  <h2>Github</h2>
+                </div>
+                <div className="applications">
+                  <img src={LinkedIn} alt="" />
+                  <h2>LinkedIn</h2>
+                </div>
+              </div>
+              <div
+                className="lower"
+                onClick={() => setStartMenuExpanded(!startMenuExpanded)}
+              ></div>
             </div>
           </motion.div>
         )}
